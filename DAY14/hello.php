99c6f238674f3c794f8bb677e1e6db3c634341a8
2101031000196-Pratik <137050690+2101031000196-Pratik@users.noreply.github.com>
2023-07-05 15:35:24 +0530

    Day 14
    form,string_fun,form using php and html

---
+++ DAY14/hello.php
@@ -0,0 +1,6 @@
+<html>
+<body>
+    hello<?php echo $_GET["name"]; ?><br>
+    Your email address is: <?php echo $_GET["email"]; ?>
+</body>
+</html>
---
+++ DAY14/if....else.php
@@ -0,0 +1,8 @@
+<?php
+$t=11;
+if ($t > "10") {
+  echo "Have a good day!";
+} else {
+  echo "Have a good night!";
+}
+?>
